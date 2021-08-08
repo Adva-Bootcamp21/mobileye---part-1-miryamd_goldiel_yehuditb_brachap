ddcaa5f8719a751e96e719133bb31e09a2989d77
goldie l <g4199912@gmail.com>
2021-08-08 13:53:52 +0300

    divid green and red matrix

--- part1_api.py
+++ part1_api.py
@@ -16,15 +16,26 @@ except ImportError:
     raise
 
 
-def find_tfl_lights(c_image: np.ndarray):
+def find_tfl_lights(c_image: np.ndarray,some_threshold):
     """
     Detect candidates for TFL lights. Use c_image, kwargs and you imagination to implement
     :param c_image: The image itself as np.uint8, shape of (H, W, 3)
     :param kwargs: Whatever config you want to pass in here
     :return: 4-tuple of x_red, y_red, x_green, y_green
     """
-    ### WRITE YOUR CODE HERE ###
-    ### USE HELPER FUNCTIONS ###
+
+    """
+    Divide into green and red matrices, find the green and red light
+    """
+    red_matrix = [[0 for i in range(1024)] for i in range(2048)]
+    green_matrix = [[0 for i in range(1024)] for i in range(2048)]
+
+    for x_cord in range(1024):
+        for y_cord in range(2048):
+            red_matrix[x_cord][y_cord] = c_image[x_cord][y_cord][0]
+            green_matrix[x_cord][y_cord] = c_image[x_cord][y_cord][1]
+
+    return ([1500,150,1200,15],[52,540,556,222],[1000,1000,1000,1000],[854,50,120,12])
 
 
 ### GIVEN CODE TO TEST YOUR IMPLENTATION AND PLOT THE PICTURES
@@ -72,7 +83,7 @@ def main(argv=None):
     parser.add_argument("-j", "--json", type=str, help="Path to json GT for comparison")
     parser.add_argument('-d', '--dir', type=str, help='Directory to scan images in')
     args = parser.parse_args(argv)
-    default_base = '../../data'
+    default_base = '../../data/leftImg8bit/test/temp'
 
     if args.dir is None:
         args.dir = default_base
